685ed38bdebdf68e65a41072ccc5dbe50109890f
Toshiaki Maki <makingx@gmail.com>
2014-03-02 21:13:17 +0900

    created

--- README.md
+++ README.md
@@ -2,3 +2,163 @@ categolj2-cli
 =============
 
 CLI frontend for CategoLJ2
+
+## Build
+
+    $ go build catego.go
+
+Show usage
+
+     $ ./catego
+      NAME:
+
+        catego
+
+      DESCRIPTION:
+
+        CLI frontend for CategoLJ2
+
+      OPTIONS:
+
+        -cfg=<path>              File path to save config. (Default: $HOME/.categolj2cfg)
+        -key=<path>              File path to save key. (Default: $HOME/.categolj2key)
+        -d=<path>                File path to store downloaded entries. (Default: .) This is used only in case of GET/POST.
+
+      COMMANDS:
+
+        clean                    Clean files.
+        rmcfg                    Remove config file.
+        refreshtoken             Refresh access token.
+
+        gets <page>              Get entries. 'page' begin with 0.
+        get <filename|entryId>   Get entry.
+        post <filename|entryId>  Create new entry.
+        put <filename|entryId>   Update the entry.
+        del <filename|entryId>   Delete the entry.
+        template                 Output template entry file.
+
+## Getting Started
+
+### Create New Entry
+
+`catego template` shows template file with some headers. You can write markdown (or HTML) file using this.
+
+     $ ./catego template
+    title: Title here
+    category: xxx::yyy::zzz
+    published: false
+    updateLastModifiedDate: false
+    saveInHistory: true
+
+    ----
+
+    Write contents here
+
+
+`catego post <filename>` creates a new entry using REST API. At the first time, you have to configure blog endpoint and isses access token.
+
+    $ ./catego template > new.md
+    $ ./catego post new.md
+    created /Users/maki/.categolj2cfg
+    enter endpoint (ex. http://blog.ik.am): http://localhost:8080
+    save /Users/maki/.categolj2cfg
+    enter your secret key to encrypt: <some secret key>
+    created /Users/maki/.categolj2key
+    enter username: <username>
+    enter password: <password>
+    save /Users/maki/.categolj2cfg
+    wrote ./<entryId>.md
+    remove new.md
+
+A new entry has created and downloaded as `<entryId>.md`. Original file has been deleted.
+
+You can change directory to download with `-d <dir>`.
+
+    $ ./catego -d foo post new.md
+    create foo
+    wrote foo/<entryId>.md
+    remove new.md
+
+### Get Entry/Entries
+
+`catego get <entryId>` downloads the specified entry.
+
+    $ ./catego get <entryId>
+    download  <entryId>
+    wrote ./<entryId>.md
+
+You can change directory to download with `-d <dir>`.
+
+    $ ./catego -d foo get 100
+    download  100
+    create foo
+    wrote foo/100.md
+
+`catego gets <page>` download entries by paging. `<page>` begin with 0 and default page is 0.
+
+    $ ./catego gets
+    download page 0
+    wrote ./251.md
+    wrote ./250.md
+    wrote ./226.md
+    wrote ./233.md
+    wrote ./232.md
+    wrote ./228.md
+    wrote ./224.md
+    wrote ./231.md
+    wrote ./230.md
+    wrote ./229.md
+
+    $ ./catego gets 1
+    download page 1
+    wrote ./225.md
+    wrote ./222.md
+    wrote ./221.md
+    wrote ./220.md
+    wrote ./219.md
+    wrote ./217.md
+    wrote ./216.md
+    wrote ./215.md
+    wrote ./214.md
+    wrote ./213.md
+
+Sure, you can specify download directory.
+
+    $ ./catego -d foo gets
+    download page 0
+    wrote foo/251.md
+    wrote foo/250.md
+    wrote foo/226.md
+    wrote foo/233.md
+    wrote foo/232.md
+    wrote foo/228.md
+    wrote foo/224.md
+    wrote foo/231.md
+    wrote foo/230.md
+    wrote foo/229.md
+
+### Update Entry
+
+`catego put <filename` updates the entry.
+
+    $ ./catego put <entryId>.md
+    wrote ./<entryId>.md
+
+You don't need to specify changed directory to download.
+
+    $ ./catego put foo/<entryId>.md
+    wrote foo/<entryId>.md
+
+### Delete Entry
+
+`catego del <filename>` deletes the entry.
+
+    $ ./catego del <entryId>.md
+    remove <entryId>.md
+
+    $ ./catego del foo/<entryId>.md
+    remove foo/<entryId>.md
+
+## License
+
+Licensed under the Apache License, Version 2.0.
--- catego.go
+++ catego.go
@@ -5,8 +5,6 @@ import (
 	"flag"
 	"strconv"
 	"strings"
-	"log"
-	"time"
 	"os"
 	"bufio"
 	"io"
@@ -18,15 +16,22 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
+	"net/url"
+	"bytes"
+	"unicode"
+	"path/filepath"
 )
 
 type Entry struct {
-	EntryId        int64 `json:"entryId"`
-	Title          string `json:"title"`
-	Contents       string `json:"contents"`
-	CreatedDate    time.Time `json:"createdDate"`
-	Format         string `json:"format"`
-	CategoryString string `json:"categoryString"`
+	EntryId                  int64 `json:"id"`
+	Title                    string `json:"title"`
+	Contents                 string `json:"contents"`
+	Format                   string `json:"format"`
+	CategoryString           string `json:"categoryString"`
+	Published                bool `json:"published"`
+	Version                  int64 `json:"version"`
+	UpdateLastModifiedDate   bool `json:"updateLastModifiedDate"`
+	SaveInHistory            bool `json:"saveInHistory"`
 }
 
 type Page struct {
@@ -44,65 +49,38 @@ type EntryPage struct {
 	Content []Entry `json:"content"`
 }
 
-func (entry* Entry) Save() {
+func (entry* Entry) Save(basedir string) bool {
+	chk, err := os.Open(basedir)
+	defer chk.Close()
+	if err != nil {
+		fmt.Println("create " + basedir)
+		os.MkdirAll(basedir, 0777)
+	}
 	if (entry.EntryId < 1) {
 		fmt.Println("cannot save")
-		return
+		return false
 	}
 
-	filename := fmt.Sprintf("%d.%s", entry.EntryId, entry.Format)
+	filename := fmt.Sprintf("%s/%d.%s", basedir, entry.EntryId, entry.Format)
 	f, _ := os.Create(filename)
 	defer f.Close()
 	f.WriteString("title: " + entry.Title + "\n")
 	f.WriteString("category: " + entry.CategoryString + "\n")
+	f.WriteString(fmt.Sprintf("published: %t\n", entry.Published))
+	f.WriteString(fmt.Sprintf("updateLastModifiedDate: %t\n", entry.UpdateLastModifiedDate))
+	f.WriteString(fmt.Sprintf("saveInHistory: %t\n", entry.SaveInHistory))
 	f.WriteString("\n")
 	f.WriteString("----\n")
 	f.WriteString("\n")
 	f.WriteString(entry.Contents)
 	f.Sync()
 	fmt.Println("wrote " + filename)
+	return true
 }
 
-func getEntries(page int) {
-	fmt.Println("download page", page)
-	var entryPage EntryPage
-	GetForEntity(fmt.Sprintf("http://blog.ik.am/api/v1/entries/?page=%d", page))(&entryPage)
-	for _, entry := range entryPage.Content {
-		entry.Save()
-	}
-}
-
-func getEntry(filename string) {
-	entryId, _ := getEntryIdAndFormat(filename)
-
-	fmt.Println("download ", entryId)
-	var entry Entry
-	GetForEntity("http://blog.ik.am/api/v1/entries/" + entryId)(&entry)
-	entry.Save()
-}
-
-func postEntry(filename string) {
-	_, format := getEntryIdAndFormat(filename)
-
-	var entry Entry;
-	entry.Format = format
-	entry.Contents, _ = readContents(filename)
-
-	log.Println(entry)
-}
-
-func putEntry(filename string) {
-	entryId, format := getEntryIdAndFormat(filename)
-
-	var entry Entry
-	var header *Header
-	entry.EntryId, _ = strconv.ParseInt(entryId, 0, 32)
-	entry.Format = format
-	entry.Contents, header = readContents(filename)
-	entry.Title = header.Get("title")
-	entry.CategoryString = header.Get("category")
-
-	log.Println(entry)
+func (entry* Entry) NewReader() io.Reader {
+	data, _ := json.Marshal(entry)
+	return bytes.NewReader(data)
 }
 
 type Header struct {
@@ -130,15 +108,114 @@ func (header *Header) Get(key string) string {
 	return header.data[strings.ToUpper(key)]
 }
 
-func GetForEntity(url string) func(v interface{}) error {
-	response, err := http.Get(url);
+func (header *Header) Apply(entry *Entry) {
+	entry.Title = header.Get("title")
+	entry.CategoryString = header.Get("category")
+	if header.Get("published") != "" {
+		published, _ := strconv.ParseBool(header.Get("published"))
+		entry.Published = published
+	} else {
+		entry.Published = false
+	}
+	if header.Get("updateLastModifiedDate") != "" {
+		updateLastModifiedDate, _ := strconv.ParseBool(header.Get("updateLastModifiedDate"))
+		entry.UpdateLastModifiedDate = updateLastModifiedDate
+	} else {
+		entry.UpdateLastModifiedDate = false
+	}
+	if header.Get("saveInHistory") != "" {
+		saveInHistory, _ := strconv.ParseBool(header.Get("saveInHistory"))
+		entry.SaveInHistory = saveInHistory
+	} else {
+		entry.SaveInHistory = false
+	}
+}
+
+func getEntries(page int, accessToken string, endpoint string, basedir string) {
+	fmt.Println("download page", page)
+	var entryPage EntryPage
+	getForEntity(fmt.Sprintf(endpoint+"/api/v1/entries/?page=%d", page), accessToken)(&entryPage)
+	for _, entry := range entryPage.Content {
+		entry.Save(basedir)
+	}
+}
+
+func getEntry(filename string, accessToken string, endpoint string, basedir string) {
+	entryId, _ := getEntryIdAndFormat(filename)
+
+	fmt.Println("download ", entryId)
+	var entry Entry
+	getForEntity(endpoint+"/api/v1/entries/"+entryId, accessToken)(&entry)
+	entry.Save(basedir)
+}
+
+func postEntry(filename string, accessToken string, endpoint string, basedir string) {
+	_, format := getEntryIdAndFormat(filename)
+
+	var entry Entry;
+	entry.Format = format
+	contents, header := readContents(filename)
+	entry.Contents = strings.TrimLeftFunc(contents, unicode.IsSpace)
+	header.Apply(&entry)
+
+	var created Entry
+	postForEntity(endpoint+"/api/v1/entries", entry, accessToken)(&created)
+
+	if created.Save(basedir) {
+		removeFile(filename)
+	}
+}
+
+func putEntry(filename string, accessToken string, endpoint string, basedir string) {
+	entryId, format := getEntryIdAndFormat(filename)
+
+	var entry Entry
+	var header *Header
+	entry.EntryId, _ = strconv.ParseInt(entryId, 0, 32)
+	entry.Format = format
+	contents, header := readContents(filename)
+	entry.Contents = strings.TrimLeftFunc(contents, unicode.IsSpace)
+	header.Apply(&entry)
+
+	var updated Entry
+	putForEntity(endpoint+"/api/v1/entries/"+entryId, entry, accessToken)(&updated)
+	updated.Save(basedir)
+}
+
+
+func deleteEntry(filename string, accessToken string, endpoint string) {
+	entryId, _ := getEntryIdAndFormat(filename)
+	deleteForEntity(endpoint+"/api/v1/entries/"+entryId, accessToken)(nil)
+	removeFile(filename)
+}
+
+func getForEntity(url string, accessToken string) func(v interface{}) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	req.Header.Add("X-Admin", "true")
+
 	if err != nil {
 		panic(err)
 	}
-	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)
+
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if (resp.StatusCode != 200) {
+		ret := map[string] string {}
+		json.Unmarshal(contents, &ret)
+		return func(v interface{}) error {
+			fmt.Println(resp.Status + " (" + ret["error_description"] + ")")
+			return nil
+		}
 	}
 
 	return func(v interface{}) error {
@@ -146,11 +223,115 @@ func GetForEntity(url string) func(v interface{}) error {
 	}
 }
 
+func postForEntity(url string, entry Entry, accessToken string) func(v interface{}) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, entry.NewReader())
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	req.Header.Add("X-Admin", "true")
+
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	if (resp.StatusCode != 201) {
+		ret := map[string] string {}
+		json.Unmarshal(contents, &ret)
+		return func(v interface{}) error {
+			fmt.Println(resp.Status + " (" + ret["error_description"] + ")")
+			return nil
+		}
+	}
+
+	return func(v interface{}) error {
+		return json.Unmarshal(contents, v)
+	}
+}
+
+func putForEntity(url string, entry Entry, accessToken string) func(v interface{}) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", url, entry.NewReader())
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	req.Header.Add("X-Admin", "true")
+
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	if (resp.StatusCode != 200) {
+		ret := map[string] string {}
+		json.Unmarshal(contents, &ret)
+		return func(v interface{}) error {
+			fmt.Println(resp.Status + " (" + ret["error_description"] + ")")
+			return nil
+		}
+	}
+
+	return func(v interface{}) error {
+		return json.Unmarshal(contents, v)
+	}
+}
+
+func deleteForEntity(url string, accessToken string) func(v interface{}) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("DELETE", url, nil)
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	req.Header.Add("X-Admin", "true")
+
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if (resp.StatusCode != 204) {
+		ret := map[string] string {}
+		json.Unmarshal(contents, &ret)
+		return func(v interface{}) error {
+			fmt.Println(resp.Status + " (" + ret["error_description"] + ")")
+			return nil
+		}
+	}
+
+	return func(v interface{}) error {
+		return nil
+	}
+}
+
 func readContents(filename string) (string, *Header) {
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(filename + " is not found!")
-		os.Exit(1)
+		panic(err)
 	}
 	reader := bufio.NewReader(f)
 
@@ -172,7 +353,9 @@ func readContents(filename string) (string, *Header) {
 	}
 	for {
 		line, err = reader.ReadString('\n')
+
 		if err == io.EOF {
+			contents += line
 			break;
 		}
 
@@ -181,29 +364,25 @@ func readContents(filename string) (string, *Header) {
 				header.ParseLine(line)
 			} else if header.IsHeaderFinished(line) {
 				isReadingHeader = false
-				fmt.Println("header ", header.data)
 			}
 			continue
 		}
+
 		contents += line
 	}
-
 
 	return contents, header
 }
 
-func deleteEntry(filename string) {
-	entryId, _ := getEntryIdAndFormat(filename)
-	log.Println("delete " + entryId)
-}
 
 func getEntryIdAndFormat(filename string) (string, string) {
-	result := strings.Split(filename, ".")
+	basename := filepath.Base(filename)
+	result := strings.Split(basename, ".")
 	size := len(result)
 	if size == 0 {
 		panic("<filename> is not specified!")
 	} else if size == 1 {
-		return filename, "md"
+		return basename, "md"
 	}
 	entryId := strings.Join(result[:size-1], ".")
 	format := result[size - 1:][0]
@@ -211,7 +390,7 @@ func getEntryIdAndFormat(filename string) (string, string) {
 }
 
 // encrypt string to base64 crypto using AES
-func Encrypt(key []byte, text string) string {
+func enceypt(key []byte, text string) string {
 	plaintext := []byte(text)
 
 	block, err := aes.NewCipher(key)
@@ -235,7 +414,7 @@ func Encrypt(key []byte, text string) string {
 }
 
 // decrypt from base64 to decrypted string
-func Decrypt(key []byte, cryptoText string) string {
+func deceypt(key []byte, cryptoText string) string {
 	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
 
 	block, err := aes.NewCipher(key)
@@ -265,11 +444,11 @@ const (
 
 func createNewKey(filename string) []byte {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("enter key to encrypt: ")
+	fmt.Print("enter your secret key to encrypt: ")
 	scanner.Scan()
 	keyString := (scanner.Text()+strings.Repeat("$", 32))[:32]
-	encryptedKey := []byte(Encrypt([]byte(MAGIC), keyString))
-	err := ioutil.WriteFile(filename, encryptedKey, 0644)
+	encryptedKey := []byte(enceypt([]byte(MAGIC), keyString))
+	err := ioutil.WriteFile(filename, encryptedKey, 0600)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -282,7 +461,7 @@ func readDecryptedKey(filename string) string {
 	if err != nil {
 		encryptedKey = createNewKey(filename)
 	}
-	return Decrypt([]byte(MAGIC), string(encryptedKey))
+	return deceypt([]byte(MAGIC), string(encryptedKey))
 }
 
 func createCfg(filename string) {
@@ -310,53 +489,231 @@ func readAccessToken(filename string, key string) string {
 	if len(accessToken) == 0 {
 		return ""
 	}
-	return Decrypt([]byte(key), accessToken)
+	return deceypt([]byte(key), accessToken)
 }
 
-func saveAccessToken(token string, filename string, key string) {
-	encryptedToken := Encrypt([]byte(key), token)
+func saveAccessToken(accessToken string, refreshToken string, filename string, key string) {
 	cfg := readCfg(filename)
-	cfg["access_token"] = encryptedToken
+	encryptedAccessToken := enceypt([]byte(key), accessToken)
+	cfg["access_token"] = encryptedAccessToken
 
+	if refreshToken != "" {
+		encryptedRefreshToken := enceypt([]byte(key), refreshToken)
+		cfg["refresh_token"] = encryptedRefreshToken
+	}
+
+	saveCfg(filename, cfg)
+}
+
+func issueAccessToken(username string, password string, endpoint string) (string, string) {
+	resp, err := http.PostForm(endpoint+"/oauth/token",
+		url.Values{
+		"username": {username},
+		"password": {password},
+		"client_id": {"categolj2-admin"},
+		"client_secret": {"categolj2-secret"},
+		"grant_type": {"password"}})
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+
+	ret := map[string] string {}
+	json.Unmarshal(contents, &ret)
+
+	if (resp.StatusCode != 200) {
+		fmt.Println(resp.Status + " (" + ret["error_description"] + ")")
+		return "", ""
+	}
+	return ret["access_token"], ret["refresh_token"]
+}
+
+func refreshToken(cfgfile string, endpoint string) {
+	cfg := readCfg(cfgfile)
+	refreshToken := cfg["refresh_token"]
+	resp, err := http.PostForm(endpoint+"/oauth/token",
+		url.Values{
+		"token": {refreshToken},
+		"client_id": {"categolj2-admin"},
+		"client_secret": {"categolj2-secret"},
+		"grant_type": {"refresh_token"}})
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+
+	ret := map[string] string {}
+	json.Unmarshal(contents, &ret)
+
+	if (resp.StatusCode != 200) {
+		fmt.Println(resp.Status + " (" + ret["error_description"] + ")")
+		fmt.Println("clear access token. please re-login.")
+		cfg["access_token"] = ""
+		cfg["refresh_token"] = ""
+		saveCfg(cfgfile, cfg)
+		return
+	}
+
+	cfg["access_token"] = ret["access_token"]
+	cfg["refresh_token"] = ret["refresh_token"]
+	saveCfg(cfgfile, cfg)
+}
+
+func removeFile(filename string) {
+	fmt.Println("remove " + filename)
+	os.Remove(filename)
+}
+
+func saveCfg(filename string, cfg map[string] string) {
 	data, _ := json.Marshal(cfg)
 	err := ioutil.WriteFile(filename, data, 0644)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println("save " + filename)
 	}
-	fmt.Println("save " + filename)
 }
 
-func main() {
-	flag.Parse()
-
-	keyfile := ".categolj2key"
-	cfgfile := ".categolj2cfg"
+func checkToken(keyfile string, cfgfile string, endpoint string) string {
 	key := readDecryptedKey(keyfile)
-	token := readAccessToken(cfgfile, key)
-	if token == "" {
+	accessToken := readAccessToken(cfgfile, key)
+	refreshToken := ""
+	if accessToken == "" {
 		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("enter access token: ")
+		fmt.Print("enter username: ")
 		scanner.Scan()
-		saveAccessToken(scanner.Text(), cfgfile, key)
+		username := scanner.Text()
+		fmt.Print("enter password: ")
+		scanner.Scan()
+		password := scanner.Text()
+
+		accessToken, refreshToken = issueAccessToken(username, password, endpoint)
+		if (accessToken == "") {
+			fmt.Println("cannot get access token")
+			return ""
+		}
+		saveAccessToken(accessToken, refreshToken, cfgfile, key)
 	}
-	fmt.Println(token)
+	return accessToken
+}
+
+func checkEndpoint(cfgfile string) string {
+	cfg := readCfg(cfgfile)
+	endpoint := cfg["endpoint"]
+	if len(endpoint) == 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("enter endpoint (ex. http://blog.ik.am): ")
+		scanner.Scan()
+		endpoint = scanner.Text()
+		cfg["endpoint"] = endpoint
+		saveCfg(cfgfile, cfg)
+	}
+	return endpoint
+}
+
+
+func main() {
+	var (
+		keyfile string
+		cfgfile string
+		basedir string
+	)
+	flag.StringVar(&keyfile, "key", os.Getenv("HOME")+"/.categolj2key", "File path to save config")
+	flag.StringVar(&cfgfile, "cfg", os.Getenv("HOME")+"/.categolj2cfg", "File path to save key")
+	flag.StringVar(&basedir, "d", ".", "File path to store downloaded entries.")
+	flag.Usage = func() {
+		fmt.Println(`
+  NAME:
+
+    catego
+
+  DESCRIPTION:
+
+    CLI frontend for CategoLJ2
+
+  OPTIONS:
+
+    -cfg=<path>              File path to save config. (Default: $HOME/.categolj2cfg)
+    -key=<path>              File path to save key. (Default: $HOME/.categolj2key)
+    -d=<path>                File path to store downloaded entries. (Default: .) This is used only in case of GET/POST.
+
+  COMMANDS:
+
+    clean                    Clean files.
+    rmcfg                    Remove config file.
+    refreshtoken             Refresh access token.
+
+    gets <page>              Get entries. 'page' begin with 0.
+    get <filename|entryId>   Get entry.
+    post <filename|entryId>  Create new entry.
+    put <filename|entryId>   Update the entry.
+    del <filename|entryId>   Delete the entry.
+    template                 Output template entry file.
+`[1:])
+	}
+	flag.Parse()
 
 	cmd := flag.Arg(0)
 	switch {
+	case cmd == "clean":
+		removeFile(keyfile)
+		removeFile(cfgfile)
+	case cmd == "rmcfg":
+		removeFile(cfgfile)
+	case cmd == "refreshtoken":
+		endpoint := checkEndpoint(cfgfile)
+		refreshToken(cfgfile, endpoint)
 	case cmd == "gets":
 		page, _ := strconv.Atoi(flag.Arg(1))
-		getEntries(page)
+		endpoint := checkEndpoint(cfgfile)
+		accessToken := checkToken(keyfile, cfgfile, endpoint)
+		if accessToken != "" {
+			getEntries(page, accessToken, endpoint, basedir)
+		}
 	case cmd == "get":
 		filename := strings.TrimSpace(flag.Arg(1))
-		getEntry(filename)
+		endpoint := checkEndpoint(cfgfile)
+		accessToken := checkToken(keyfile, cfgfile, endpoint)
+		if accessToken != "" {
+			getEntry(filename, accessToken, endpoint, basedir)
+		}
 	case cmd == "post":
 		filename := strings.TrimSpace(flag.Arg(1))
-		postEntry(filename)
+		endpoint := checkEndpoint(cfgfile)
+		accessToken := checkToken(keyfile, cfgfile, endpoint)
+		if accessToken != "" {
+			postEntry(filename, accessToken, endpoint, basedir)
+		}
 	case cmd == "put":
 		filename := strings.TrimSpace(flag.Arg(1))
-		putEntry(filename)
+		endpoint := checkEndpoint(cfgfile)
+		accessToken := checkToken(keyfile, cfgfile, endpoint)
+		if accessToken != "" {
+			putEntry(filename, accessToken, endpoint, filepath.Dir(filename))
+		}
 	case cmd == "del":
 		filename := strings.TrimSpace(flag.Arg(1))
-		deleteEntry(filename)
+		endpoint := checkEndpoint(cfgfile)
+		accessToken := checkToken(keyfile, cfgfile, endpoint)
+		if accessToken != "" {
+			deleteEntry(filename, accessToken, endpoint)
+		}
+	case cmd == "template":
+		fmt.Println(`
+title: Title here
+category: xxx::yyy::zzz
+published: false
+updateLastModifiedDate: false
+saveInHistory: true
+
+----
+
+Write contents here`[1:])
+	default:
+		flag.Usage()
 	}
 }
